e05e9d98c5f18f3714f71389e0978c79d41729d7
kewp <karlpenzhorn@gmail.com>
2024-04-29 12:50:37 +0800

    trying to get the tailwind styles to work. looks like it is just picking them all up from uno.css

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+// import tailwind from "$fresh/plugins/tailwind.ts";
+import twindPlugin from "$fresh/plugins/twindv1.ts"
+import twindConfig from "./twind.config.ts";
 
 export default defineConfig({
-    plugins: [tailwind()]
+    // plugins: [tailwind()]
+    plugins: [twindPlugin(twindConfig)]
 });
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { type PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts"
 export default function App({ Component }: PageProps) {
   return (
     <html>
@@ -7,6 +8,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>shadcn-fresh-example</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link href={asset("/uno.css")} rel="stylesheet" />
       </head>
       <body>
         <Component />
