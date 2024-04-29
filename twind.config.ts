0923d48931ba009a378a188c291ca8ff5c2d0a53
kewp <karlpenzhorn@gmail.com>
2024-04-29 15:11:55 +0800

    starting to understand how styles work. got text-muted-foreground working

--- twind.config.ts
+++ twind.config.ts
@@ -3,8 +3,22 @@ import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
 export default {
-  ...defineConfig({
-    presets: [presetTailwind() as Preset, presetAutoprefix()],
-  }),
-  selfURL: import.meta.url,
+    ...defineConfig({
+        presets: [presetTailwind() as Preset, presetAutoprefix()],
+        theme: {
+            extend: {
+                colors: {
+                    primary: {
+                        DEFAULT: "hsl(var(--primary))",
+                        foreground: "hsl(var(--primary-foreground))",
+                    },
+                    muted: {
+                        DEFAULT: "hsl(var(--muted))",
+                        foreground: "hsl(var(--muted-foreground))",
+                    },
+                },
+            },
+        },
+    }),
+    selfURL: import.meta.url,
 };
